7d33f55d33ee86b3ab8c466979225439575808a6
root <root@PSC0155.popular-soft.local>
2021-04-24 18:39:40 +0900

    edit for public

--- testcode_module/postslacktest.py
+++
@@ -1,7 +0,0 @@
-import slackweb
-import os
-import requests
-from common_module import postslack
-
-def execute():
-    postslack.SendToSlackMessage("test")
--- testparts_module/kokyakuopen.py
+++
@@ -1,4 +0,0 @@
-from common_module import move
-
-def excute(driver):
-    move.excute(driver,"/html/body/app-root/app-main-layout/div/sa-navigation/aside/nav/ul/li[3]/a/span")
