3897a2b036f34fc835ada08535e5da9b7103f9ec
shoma-mano <65682253+shoma-mano@users.noreply.github.com>
2021-04-24 19:02:21 +0900

    edit

--- common_module/reportexcel.py
+++ common_module/reportexcel.py
@@ -1,42 +1,30 @@
-import openpyxl
-from openpyxl.drawing.image import Image
-import datetime
-from common_module import postslackreport
+#coding: UTF-8
 
-#日付取得
-dt_now=datetime.datetime.now()
-month=str(dt_now.month)
-day=str(dt_now.day)
-hour=str(dt_now.hour)
-minute=str(dt_now.minute)
-if len(minute)==1:
-    minute="0"+minute
+import slackweb
+import os
+import requests
 
-def excute(data):
-    # ワークブック読み込み
-    book = openpyxl.load_workbook("excel//ResultTemplate.xlsx")
+def SendToSlack(message):
+ 
+    # Slackへの投稿に必要は情報を設定
+    token = 'your token'
+    channel = 'your channel'
+    text = message
+ 
+    param ={
+        'token': token,
+        'channels': 'your channels',
+        'initial_comment': text
+      }
+ 
+    # 投稿するExcelファイルを絶対パスを取得
+    fullpath = os.path.join("excelpath", "message")
+    files = {'file': open(fullpath, 'rb')}
+ 
+    # SlackへExcelファイルを投稿
+    r=requests.post(url='https://slack.com/api/files.upload', params=param, files=files)
+    print(r.url)
+    print(r.text)
+    print(message+"のエクセルをSlackに投稿しました")
 
-    # シートを取得し名前を決定
-    ws = book.worksheets[0]
-    ws.title = "実施結果"
-    
-    #実施日時タイトル書き込み
-    title=ws["A2"]
-    title.value=month+"月"+day+"日 "+hour+":"+minute+" 自動テスト実施結果"
-
-    #書き込む行のセル取得
-    cells = ws['B7':'E10']
-
-    for index,row in enumerate(cells):
-        for ind,cell in enumerate(row):
-            try:
-              cell.value=data.result[(ws[cell.row][0]).value][(ws[5][cell.column-1]).value]
-            except:
-                pass
-
-
-    book.save("excel\\"+"実施結果"+'.xlsx')
-    print("エクセルを作成しました")
-
-    postslackreport.SendToSlack("実施結果")
 
